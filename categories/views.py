30ad2805cffa543ae20994a2120e263d6530e2c7
Abdoulaye NDAO <abdoulayendao12@gmail.com>
2021-07-18 20:47:33 +0000

    feat:Implementation of categoryt views version 1

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from .serializers import CategorySerializer
 
 # Create your views here.
+    
+# Create your views here.
+@csrf_exempt
+def CategoryApi(request,id=0):
+    if request.method=='GET':
+       
+        return JsonResponse({}, safe=False)  
